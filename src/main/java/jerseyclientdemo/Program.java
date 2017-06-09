6d0fc9114fb1ecf1c6eaeb79caf09668daaebb35
Adam Urbanowicz <adam.urbanow@gmail.com>
2017-06-09 23:24:09 +0200

    initial commit

---
+++ src/main/java/jerseyclientdemo/Program.java
@@ -0,0 +1,18 @@
+package jerseyclientdemo;
+
+import java.util.List;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+	}
+	
+	
+}
